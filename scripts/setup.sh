0cf0061e3ad780a3c852c4f0827514ea5846ed43
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-06-04 17:29:59 +0100

    only get env file if not there

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -37,11 +37,13 @@ checkIfAbleToTalkToAWS() {
 }
 
 fetchEnv() {
-  if [[ ${STAGE} != "PROD" ]]; then
-      aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json --profile media-service
-  else
-      aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json
-  fi
+    if [[ ! -f ${DIR}/../env.json ]]; then
+        if [[ ${STAGE} != "PROD"  ]]; then
+            aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json --profile media-service
+        else
+            aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json
+        fi
+    fi
 }
 
 checkForNodeModules
