e2930dc89ad968ae3d645fc4e48043d5a6e8ab6a
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-06-04 18:30:55 +0100

    Get cookie from within npm script

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,8 +8,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FAILURES_FILE="${DIR}/../failures.txt"
 
 ${DIR}/setup.sh "${STAGE}"
-STAGE="${STAGE}" node "${DIR}"/../src/utils/cookie.js
-
 echo "$(date): Running integration tests"
 
 pushd "${DIR}"/../ > /dev/null
