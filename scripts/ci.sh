7adbe1289736db65ccf1802c7dc8aa4c194427fc
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-08-07 13:05:38 +0100

    Add TeamCity script hints (#59)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+echo "##teamcity[blockOpened name='npm']"
 npm ci
+echo "##teamcity[blockClosed name='npm']"
+
+echo "##teamcity[testSuiteStarted name='lint']"
 npm run lint
+echo "##teamcity[testSuiteFinished name='lint']"
+
+echo "##teamcity[compilationStarted compiler='riffraff']"
 npm run riffraff
+echo "##teamcity[compilationFinished compiler='riffraff']"
+
