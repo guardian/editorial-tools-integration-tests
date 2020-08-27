55520150805cf5a4aa08af8b4efa9ab5ee9fcf84
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-08-27 10:22:08 +0100

    Refactor Workflow tests

---
+++ cypress/utils/workflow/utils.ts
@@ -0,0 +1,43 @@
+import { getDomain } from '../networking';
+import { defaultQueryString } from '../../integration/workflow/integration';
+
+export const visitWorkflow = (params = '') => {
+  const waitAlias =
+    '@content' +
+    (params === `/dashboard${defaultQueryString}` ? 'WithDefaultQuery' : '');
+
+  cy.visit(getDomain({ app: 'workflow' }) + params)
+    .wait(waitAlias)
+    .get('.wf-loader', { timeout: 30000 })
+    .should('not.exist');
+};
+
+export function createArticle(title: string) {
+  cy.get('[wf-dropdown-toggle]').contains('Create new').click();
+  cy.get('#testing-dashboard-create-dropdown-Article').click();
+  cy.get('#stub_title').type(title);
+  cy.get('#stub_section').select('Training');
+  cy.get('#testing-create-in-composer').click();
+  cy.get('.modal-dialog')
+    .contains('Completed successfully!')
+    .should('be.visible')
+    .get('.alert-danger')
+    .should('not.be.visible')
+    .get('.close')
+    .click()
+    .wait('@stubs');
+}
+
+export function searchFor(query: string) {
+  cy.get('#testing-dashboard-toolbar-section-search').type(query + '{enter}');
+}
+
+export function clickOnArticle(title: string) {
+  cy.wait('@searchForText')
+    .wait(2000)
+    .get('#testing-content-list-item-title-anchor-text')
+    .contains(title)
+    .should('exist')
+    .parent()
+    .click();
+}
