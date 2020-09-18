e52c988f953b68e4ac8b9c8e4a2adcda7f75e23f
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-09-18 12:22:29 +0100

    Add GHA and update fetch-config

--- scripts/fetch-config.sh
+++ scripts/fetch-config.sh
@@ -5,7 +5,10 @@ set -e
 STAGE=${1:-LOCAL}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [[ ${STAGE} != "PROD"  ]]; then
+if [[ $GITHUB_ACTIONS = true ]]; then
+    echo Fetching config with envar credentials
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json
+elif [[ ${STAGE} != "PROD"  ]]; then
     aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json --profile media-service
 else
     aws s3 cp s3://editorial-tools-integration-tests-dist/env.dev.json ${DIR}/../env.json
