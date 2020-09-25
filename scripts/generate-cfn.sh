6ceb828c03ecf8b4d229d7e8f4754d6981a6f143
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-09-25 12:12:27 +0100

    Actually delete CDK dir, remove package-lock.json

--- scripts/generate-cfn.sh
+++ scripts/generate-cfn.sh
@@ -8,6 +8,6 @@ OUTPUT_DIR="${ROOT_DIR}/cloudformation/cdk"
 
 pushd "${ROOT_DIR}"/cdk || exit
 yarn --silent
-rm "${OUTPUT_DIR}" || true
+rm -r "${OUTPUT_DIR}" || true
 yarn cdk synth -o "${OUTPUT_DIR}" > /dev/null
 popd
