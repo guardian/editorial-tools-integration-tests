a50168712ff926f6bbbbec43a9a05cfffc53be36
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-09-28 16:19:59 +0100

    Add cloudformation to RiffRaff deployments (#94)
    * add cfn to riffraff deploy
    
    * Fix cfn name

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+ROOT_DIR="${DIR}/.."
+
 echo "##teamcity[blockOpened name='npm']"
 yarn
 echo "##teamcity[blockClosed name='npm']"
@@ -9,6 +12,10 @@ echo "##teamcity[blockClosed name='npm']"
 echo "##teamcity[testSuiteStarted name='lint']"
 yarn lint
 echo "##teamcity[testSuiteFinished name='lint']"
+
+echo "##teamcity[blockOpened name='generate-cfn']"
+"${ROOT_DIR}/scripts/generate-cfn.sh"
+echo "##teamcity[blockClosed name='generate-cfn']"
 
 echo "##teamcity[compilationStarted compiler='riffraff']"
 yarn riffraff
