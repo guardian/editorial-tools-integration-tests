b4d3be9eb3721589024aaefebb3bf27ebeb3442b
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-10-01 12:25:23 +0100

    Various improvements:
    * Abstract stack name wherever possible to make it easier to change in the future
    * Always install node modules via yarn (as it takes no time to run if already installed)
    * Remove unused npm scripts (now that `scripts/start.sh` is standard)
    * Update README

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,32 +4,25 @@ set -e
 
 STAGE=${1:-LOCAL}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+ACCOUNT="media-service"
 
 bold='\x1B[0;1m'
 green='\x1B[0;32m'
 red='\x1B[0;31m'
 plain='\x1B[0m' # No Color
 
-checkForNodeModules() {
-  if [[ ! -d ${DIR}/../node_modules ]]; then
-    echo -e "${red}No node_modules found, please run yarn.${plain}"
-    exit 1
-  fi
-}
-
 checkIfAbleToTalkToAWS() {
   if [[ ${STAGE} != "PROD" ]]; then
-    STATUS=$(aws sts get-caller-identity --profile media-service 2>&1 || true)
+    STATUS=$(aws sts get-caller-identity --profile ${ACCOUNT} 2>&1 || true)
   else
     STATUS=$(aws sts get-caller-identity 2>&1 || true)
   fi
 
   if [[ ${STATUS} =~ (ExpiredToken) ]]; then
-    echo -e "${red}Credentials for the media-service profile are expired. Please fetch new credentials and run this again.${plain}"
+    echo -e "${red}Credentials for the ${ACCOUNT} profile are expired. Please fetch new credentials and run this again.${plain}"
     exit 1
   elif [[ ${STATUS} =~ ("could not be found") ]]; then
-    echo -e "${red}Credentials for the media-service profile are missing. Please ensure you have the right credentials.${plain}"
+    echo -e "${red}Credentials for the ${ACCOUNT} profile are missing. Please ensure you have the right credentials.${plain}"
     exit 1
   fi
 }
@@ -40,6 +33,6 @@ fetchEnv() {
     fi
 }
 
-checkForNodeModules
 checkIfAbleToTalkToAWS
+yarn --silent # install node dependencies
 fetchEnv
